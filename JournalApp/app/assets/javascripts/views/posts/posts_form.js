c07e35fff19d2a3cc595448868a43b5b046c8375
App Academy Student <student@appacademy.io>
2015-06-03 15:33:53 -0700

    Finish PostsShow and add PostForm skeleton

---
+++ JournalApp/app/assets/javascripts/views/posts/posts_form.js
@@ -0,0 +1,11 @@
+JournalApp.Views.PostForm = Backbone.View.extend({
+  template: JST['posts/post_form'],
+
+  intialize: function (options) {
+
+  },
+
+  render: function () {
+
+  }
+});
--- JournalApp/app/assets/javascripts/views/posts/posts_index.js
+++ JournalApp/app/assets/javascripts/views/posts/posts_index.js
@@ -3,9 +3,7 @@ JournalApp.Views.PostsIndex = Backbone.View.extend({
   tagName: 'ul',
 
   initialize: function (options) {
-    // this.$el = $('#main').find('ul');
     this.collection = options.collection;
-    this.$rootEl = options.$rootEl;
 
     this.collection.fetch({ reset: true });
 
@@ -20,7 +18,6 @@ JournalApp.Views.PostsIndex = Backbone.View.extend({
       var view = new JournalApp.Views.PostsIndexItem({ model: post });
       this.$el.append(view.render().$el);
     }.bind(this));
-    this.$rootEl.html(this.$el);
     return this;
   }
 
